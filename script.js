eeab3119f724bd23f8634b28a6b16cf8eca35c43
Muralimano-Jayakumar <76448459+Muralimano-Jayakumar@users.noreply.github.com>
2021-04-11 10:06:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const startinpoint =6;
+
+let time =startinpoint *60;
+
+const countdown = document.getElementById('counter');
+
+setInterval(updateCounter, 1000)
+
+function updateCounter() { 
+    const minutes =Math.floor(time / 60);
+    let seconds =time % 60;
+
+    seconds = seconds < 10 ? "0" + seconds : seconds
+
+    countdown.innerHTML = `${minutes}: ${seconds}`;
+    time !== 0 ? time -- : time;
+}
+
+function restart(){
+
+    window.location.reload();
+}
